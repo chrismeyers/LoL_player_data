ab6abc40b015c02d1689e96faef607b9d1a84d39
christopher-meyers <cm.02.93@gmail.com>
2014-07-13 20:16:16 -0400

    fixed simplemodal for ie.

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@ echo "<div id='footer'>
             <a id='simplemodal-legal' class='footer-links'>legal</a>
         </div>
         
-        <div id='about' style='display:none;'>
+        <div id='about' style='margin-top: 50px; display:none;'>
             <h1 style='text-align: center;'>about</h1>
             This is a summer project that I am working on based on the video
             game League of Legends. It is very much in development. Currently,
@@ -15,17 +15,18 @@ echo "<div id='footer'>
             player will be parsed and presented to you. This information
             includes: summoner name, level, region, icon and a listing of all
             game mode stats that the player has earned.<br /><br />
-            <a class='simplemodal-close'>close</a>
+            <div style='text-align: center;'><a class='simplemodal-close'>close</a></div>
         </div>
-        <div id='legal' style='display:none;'>
+        <div id='legal' style='margin-top: 50px; display:none;'>
             <h1 style='text-align: center;'>legal</h1>
             LoL_player_data isn’t endorsed by Riot Games and doesn’t reflect the
             views or opinions of Riot Games or anyone officially involved in 
             producing or managing <i>League of Legends</i>. <i>League of Legends</i>
             and Riot Games are trademarks or registered trademarks of Riot
-            Games, Inc. <i>League of Legends</i> &COPY; Riot Games, Inc.
+            Games, Inc. <i>League of Legends</i> &COPY; Riot Games, Inc.<br /><br />
+            <div style='text-align: center;'><a class='simplemodal-close'>close</a></div>
         </div>
-        
+
         <script>
             $('#simplemodal-about').click(function() {
                 $('#about').modal({
